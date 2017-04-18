8c358095016647cb4b6667ec2d8d538be77f0cd6
Carlita <carlita@Macs-MacBook-Pro.local>
2017-04-18 11:51:56 -0400

    Second comment

--- python101.py
+++ python101.py
@@ -1,6 +1,8 @@
 # print "Hello, World!"
-# You can print two different thing on the same line
+# You can print two different things on the same line
 print ("Hello, World" , "Again")
+
+#This will work
 print """This
 will ignore the new lines
 unitl it sees another
