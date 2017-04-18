c77989a1b78f1c5ef0112ac5ec4606cc839b8b91
Carlita <carlita@Macs-MacBook-Pro.local>
2017-04-18 11:31:39 -0400

    This is my first commit

---
+++ python101.py
@@ -0,0 +1,7 @@
+# print "Hello, World!"
+# You can print two different thing on the same line
+print ("Hello, World" , "Again")
+print """This
+will ignore the new lines
+unitl it sees another
+three quotes """
